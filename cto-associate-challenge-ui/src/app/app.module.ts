717cf457086015d5aaa8699d516b29b331ca489d
Vaibhav <vaibhav.dalavi@gmail.com>
2018-08-03 16:27:41 +0530

    Ui Change

--- cto-associate-challenge-ui/src/app/app.module.ts
+++ cto-associate-challenge-ui/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
